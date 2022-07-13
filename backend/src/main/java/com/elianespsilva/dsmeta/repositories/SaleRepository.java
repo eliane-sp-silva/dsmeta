08ca1f1df5d3090260245a9de0e9349eb981fafc
= <3lian3.pereira@gmail.com>
2022-07-13 20:23:56 -0300

    API test

---
+++ backend/src/main/java/com/elianespsilva/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.elianespsilva.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.elianespsilva.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
---
+++ backend/src/main/java/com/elianespsilva/dsmeta/services/SaleService.java
@@ -0,0 +1,21 @@
+package com.elianespsilva.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.elianespsilva.dsmeta.entities.Sale;
+import com.elianespsilva.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale> findSales() {
+		return repository.findAll();
+	}
+	
+}
